fa8299811f1cb9b7c0c5445afe09a0c9029729da
flaviogustavorj <flaviogustavorj@gmail.com>
2026-04-01 22:02:06 -0300

    adjusted to json and better show errors

--- src/app/components/cadastrar-cliente/cadastrar-cliente.ts
+++ src/app/components/cadastrar-cliente/cadastrar-cliente.ts
@@ -23,14 +23,21 @@ export class CadastrarCliente {
   cadastrar() {
     
     //capturando os dados do formulário
-    const params = new HttpParams()
-      .set('nome', this.formulario.value.nome!) //capturando o campo nome do formulário
-      .set('cpf', this.formulario.value.cpf!);  //capturando o campo cpf do formulário
+    const request = {
+      nome: this.formulario.value.nome!,
+      cpf: this.formulario.value.cpf!
+    }
 
     //enviando os dados para o backend
-    this.http.post('http://localhost:8080/api/cliente/criar', null, { params: params, responseType: 'text' })
-      .subscribe((resposta) => { //aguardando o retorno da API
-        console.log(resposta);
+    this.http.post('http://localhost:8080/api/cliente/criar', request, { responseType: 'text' })
+      .subscribe({
+        next : (resposta) => {
+          alert(resposta);
+          this.formulario.reset();
+        },
+        error: (e) => {
+          alert('Erro: ' + e.error);
+        }
       });
   }
 
